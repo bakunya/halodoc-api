dfeaedfaaa204bc07a1b9b0536b8a53edcea765e
bakunya <bakunya@nyaan.kawaii>
2022-01-31 12:47:53 +0700

    addlogging

--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -5,6 +5,7 @@ const { stripHtml } = require('../lib/stripHtml');
 async function articlesController(req, res, next) {
     try {
         const url = `${ROOT_URL}/api/cms/articles?per_page=8&channel=general&status=published&page_no=${req.params.count}`
+        console.log(url)
         const similar = await axios.get(url)
         similar?.data?.result?.forEach(itm => stripHtml(itm.summary).result)
         return res.status(200).json({
