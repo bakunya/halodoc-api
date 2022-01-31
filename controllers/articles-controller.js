57d16e068768cd67046acd21fe0585c7546dad06
bakunya <bakunya@nyaan.kawaii>
2022-01-31 13:16:02 +0700

    update

--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -6,7 +6,7 @@ async function articlesController(req, res, next) {
     try {
         const url = `${ROOT_URL}/api/cms/articles?per_page=8&channel=general&status=published&page_no=${req.params.count}`
         console.log(url)
-        const similar = await axios.get(url)
+        const similar = await axios.get(url, null, { 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Mobile Safari/537.36' })
         similar?.data?.result?.forEach(itm => stripHtml(itm.summary).result)
         return res.status(200).json({
             results: similar.data.result,
